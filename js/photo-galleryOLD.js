14c51c88bdec391f776b6d7ce40abf967a7c88c5
Matěj <mckillem@icloud.com>
2017-06-24 12:15:34 +0200

    Funkční galerie

---
+++ js/photo-galleryOLD.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){        
+	$('li img').on('click',function(){
+		var src = $(this).attr('src');
+
+		// load folder with pictures
+		var folder = "pictures/";
+		var img = "";
+
+		$.ajax({
+			url : folder,
+			success: function (data) {
+				$(data).find("a").attr("href", function (i, val) {
+					if( val.match(/\.(jpe?g|JPG)$/) ) { 
+						img = "<img src='" + val +"' class='img-responsive'>";
+
+						var index = $(this).parent('li').index();   
+				console.log(img);
+
+						var html = '';
+						html += img;                
+						html += '<div style="height:25px;clear:both;display:block;">';
+						html += '<a class="controls next" href="'+ (index+2) + '">next &raquo;</a>';
+						html += '<a class="controls previous" href="' + (index) + '">&laquo; prev</a>';
+						html += '</div>';
+						
+						console.log(html);
+
+						$('#myModal').modal();
+						$('#myModal').on('shown.bs.modal', function(){
+							$('#myModal .modal-body').html(html);
+							//new code
+							$('a.controls').trigger('click');
+						})
+						$('#myModal').on('hidden.bs.modal', function(){
+							$('#myModal .modal-body').html('');
+						});
+						
+					} 
+				});
+			}
+		});
+
+		// var img = '<img src="" class="img-responsive"/>';
+		//start of new code new code
+		
+		
+		
+		
+   });	
+})
+        
+         
+$(document).on('click', 'a.controls', function(){
+	var index = $(this).attr('href');
+	var src = $('ul.row li:nth-child('+ index +') img').attr('src');             
+	
+	$('.modal-body img').attr('src', src);
+	
+	var newPrevIndex = parseInt(index) - 1; 
+	var newNextIndex = parseInt(newPrevIndex) + 2; 
+	
+	if($(this).hasClass('previous')){               
+		$(this).attr('href', newPrevIndex); 
+		$('a.next').attr('href', newNextIndex);
+	}else{
+		$(this).attr('href', newNextIndex); 
+		$('a.previous').attr('href', newPrevIndex);
+	}
+	
+	var total = $('ul.row li').length + 1; 
+	//hide next button
+	if(total === newNextIndex){
+		$('a.next').hide();
+	}else{
+		$('a.next').show()
+	}            
+	//hide previous button
+	if(newPrevIndex === 0){
+		$('a.previous').hide();
+	}else{
+		$('a.previous').show()
+	}
+	
+	
+	return false;
+});
